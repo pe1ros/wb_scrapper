b3919ddeeeb9898fa9d3bf525373f06ca9fe84d1
Ilia Petrosian <im.clean@yandex.ru>
2024-04-17 23:32:25 +0300

    Add condition for break loop when we navigate to mirror URL porducts

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 
+let oppenedCards = [];
+
 async function scrollToBottom(page, pageIndex) {
+  await page.evaluate(() => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
+  });
+
   console.log(`<======= SCROLL PAGE =======> ${pageIndex}`);
   await page.evaluate(async () => {
-    await new Promise((resolve, reject) => {
+    await new Promise((resolve, _) => {
       const scrollHeight = document.documentElement.scrollHeight;
       const distance = 100;
       let count = 0;
@@ -17,12 +27,43 @@ async function scrollToBottom(page, pageIndex) {
           clearInterval(interval);
           resolve();
         }
-      }, 200);
+      }, 100);
     });
   });
 }
 
-async function autoSearchAndScroll() {
+async function openRandomCard(page) {
+  await page.evaluate(() => {
+    const productCard = document.querySelector('.product-card__link');
+    if (productCard) {
+      productCard.click();
+    }
+  });
+  await page.evaluate(() => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
+  });
+  const url = await page.url();
+  if (oppenedCards.length && oppenedCards.findIndex(e => e === url) !== -1) {
+    await page.goBack();
+    return false;
+  }
+  console.log('<======= OPEN PRODUCT CARD =======>', url);
+  oppenedCards.push(url);
+  await page.goBack();
+  return true;
+}
+
+async function hasCards (page) {
+  const productCards = await page.$$('.product-card');
+  const productCardsWr = await page.$$('.product-card__wrapper');
+  return productCards.length > 3 || productCardsWr.length > 3;
+}
+
+async function autoSearchAndWatchResults() {
   console.log('<======= LAUNCH BROWSER =======>');
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
@@ -49,20 +90,30 @@ async function autoSearchAndScroll() {
     const match = url.match(/page=(\d+)/);
     const index = match ? parseInt(match[1]) : 'UNKNOWN_MAYBE_1';
 
+    // if(!hasCards(page)) {
+    //   continue;
+    // }
+
     if(prevIndex === index) {
       break;
     }
     prevIndex = index;
 
-    await scrollToBottom(page, index);
+    if (typeof prevIndex === 'number' && prevIndex % 3 == 0) {
+      const success = await openRandomCard(page);
+      if(!success) {
+        console.log('<======= BREAK LOOP CAUSE MIRROR URL =======>');
+        break;
+      }
+    } else {
+      await scrollToBottom(page, index);
+    }
 
     await page.evaluate(() => {
       const nextPageButton = document.querySelector('.pagination__next');
       if (nextPageButton) {
         nextPageButton.click();
-        return true;
       }
-      return false;
     });
   }
 
@@ -70,4 +121,4 @@ async function autoSearchAndScroll() {
   await browser.close();
 }
 
-autoSearchAndScroll();
+autoSearchAndWatchResults();
